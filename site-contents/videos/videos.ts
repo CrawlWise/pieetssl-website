9320746eb3fb198bbde345c1fa062532a32d389b
ayomideayorinde <ayorindeayomidemichael@gmail.com>
2026-03-25 14:05:06 +0100

    Changed Video on the homepage and changed one image

--- site-contents/videos/videos.ts
+++ site-contents/videos/videos.ts
@@ -2,5 +2,7 @@ import { VideoShowType } from "@/site-contents/videos/videosType";
 export const homeVideoShow: VideoShowType = {
     title: "Take a Virtual Tour of Our Senior Living Community",
     description: "Explore our welcoming environment, comfortable accommodations, and vibrant community life through our video gallery. See firsthand how we provide exceptional care and a warm atmosphere for our residents.",
-    url: "https://www.youtube.com/embed/dQw4w9WgXcQ"
+    url: "https://www.youtube.com/embed/tQYVpJPVRRQ"
 }
+
+//Previous url: "https://www.youtube.com/embed/dQw4w9WgXcQ"
